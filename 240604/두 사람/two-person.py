9a8ebbed13cddc824d2b47f463e56cf29d2e2cfe
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-04 09:57:41 +0900

    Title: 두 사람, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240604/두 사람/two-person.py
@@ -0,0 +1,12 @@
+a, se = map(str, input().split())
+b, ex = map(str, input().split())
+
+a = int(a)
+b = int(b)
+
+if a > 19 and se=="M":
+    print(1)
+elif b > 19 and se =="M":
+    print(1)
+else:
+    print(0)

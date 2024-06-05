63c0b2f3a3e2480597a665f7c7755cf0775399f3
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-05 10:46:54 +0900

    Title: 공약수의 유무, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240605/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240605/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -2,7 +2,7 @@ a, b = map(int, input().split())
 flag = False
 
 for i in range(a, b+1):
-    if 1920 % i == 0 or 2880 % i == 0:
+    if 1920 % i == 0 and 2880 % i == 0:
         print(1)
         flag = True
 

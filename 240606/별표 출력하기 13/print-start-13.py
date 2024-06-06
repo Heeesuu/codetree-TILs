3900aa7f06015aa7f0c671043f9730937a226937
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-06 17:02:27 +0900

    Title: 별표 출력하기 13, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 240606/별표 출력하기 13/print-start-13.py
+++ 240606/별표 출력하기 13/print-start-13.py
@@ -11,15 +11,26 @@ for i in range(1, n+1):
             print("*", end =' ')
     print()
 
-for i in range(1, n+1):
-    if i % 2 == 1:
-        for j in range(cnt):
-            print("*", end =' ')
-    elif n == 2:
-        print("* *")
-    else:
-        for j in range(n-cnt):
-            print("*", end =' ')
-        cnt += 1
-        
-    print()
+if n % 2 == 0:
+    for i in range(1, n+1):
+        if i % 2 == 1:
+            for j in range(cnt):
+                print("*", end =' ')
+            cnt -= 1
+        else:
+            for j in range(n-cnt):
+                print("*", end =' ')
+            
+        print()
+else:
+    for i in range(1, n+1):
+        if i % 2 == 1:
+            for j in range(cnt):
+                print("*", end =' ')
+            
+        else:
+            for j in range(n-cnt):
+                print("*", end =' ')
+            cnt += 1
+            
+        print()

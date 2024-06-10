c005bfa9cf2e6df430e2347a5fb79eb8817e38e7
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-10 14:07:39 +0900

    Title: 나눗셈의 나머지, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240610/나눗셈의 나머지/remainder-of-division.py
+++ 240610/나눗셈의 나머지/remainder-of-division.py
@@ -4,13 +4,12 @@ answer =[1] * b
 
 while a != 0:
     ans = a%b
-    if ans == 0:
-        answer[ans] *= 1
-    else:
-        answer[ans] *= 2
+    answer[ans] *= 2
     a = a // b
 
 for i in range(len(answer)):
+    if i == 0:
+        answer[i] = 1
     if i != 0 and answer[i] == 1:
         answer[i] = 0 
     count += answer[i]

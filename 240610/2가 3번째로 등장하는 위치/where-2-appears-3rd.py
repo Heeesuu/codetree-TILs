3857dc882bfbe042a3578b3b12b8e06692fbea48
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-10 14:38:14 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240610/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240610/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -5,8 +5,8 @@ answer =list(map(int, input().split()))
 count = 0
 
 for i in range(len(answer)):
-    if count == 3:
-        print(i)
-        break
     if answer[i]== 2:
         count += 1
+        if count == 3:
+            print(i+1)
+            break

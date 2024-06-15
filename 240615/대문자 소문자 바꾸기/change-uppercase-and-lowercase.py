d36894d5d549b56bd25b917cc92240dac98c15f6
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-15 18:08:48 +0900

    Title: 대문자 소문자 바꾸기, Time: 242ms, Memory: 24MB, Status: Passed - CodeTree

--- 240615/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
+++ 240615/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
@@ -1,11 +1,3 @@
 a = input()
-b = []
-
-for i in a :
-    if i.isupper():
-        b.append(i.lower())
-    else:
-        b.append(i.upper())
-
-for i in b:
-    print(i, end='')
+b = a.swapcase()
+print(b)

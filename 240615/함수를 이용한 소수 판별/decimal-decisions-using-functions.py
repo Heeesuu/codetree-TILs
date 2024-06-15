91a2c07da61fc5ebe0b88fee9e5a2333b2ab6613
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-15 21:35:32 +0900

    Title: 함수를 이용한 소수 판별, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240615/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240615/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,14 +1,13 @@
 def isPrime(n):
+    if n == 1:
+        return False
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
 
 a, b = map(int, input().split())
-if a == 1:
-    sum = 1
-else:
-    sum = 0
+sum = 0
 
 for i in range(a, b+1):
     if isPrime(i):

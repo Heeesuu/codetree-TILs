049f52d81d1de828e4c84596bd95b66a768c92b2
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-16 16:37:36 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240616/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,21 @@
+def susu(arr, brr, index ,blength):
+    for i in range(blength):
+        if arr[index] != brr[i]:
+            return False
+        else:
+            index += 1
+    return True
+
+
+a, b = map(int, input().split())
+
+aarr= list(map(int, input().split()))
+barr = list(map(int, input().split()))
+bleng = len(barr)
+
+for i in range(len(aarr)):
+    if aarr[i] == barr[0]:
+        if susu(aarr, barr, i, bleng):
+            print("Yes")
+        else:
+            print("No")

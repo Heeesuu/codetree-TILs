efd411a3722d4efb1ba44ddf9a5c45ca70735b6a
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-16 16:25:34 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240616/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 240616/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -1,11 +1,12 @@
 def decimal(n):
     count = 0
     for i in range(2, n+1):
-        if count > 1:
-            return False
         if n % i == 0:
             count += 1
-    return True
+    if count > 1:
+        return False
+    else:
+        return True
 
 def even(n):
     a = 0

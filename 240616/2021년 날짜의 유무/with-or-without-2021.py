f5b87735f1a101b9a8df005db410d394b589068f
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-16 17:17:58 +0900

    Title: 2021년 날짜의 유무, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240616/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,36 @@
+def d31(n):
+    if n <= 31 and n > 0:
+        return True
+    else:
+        return False
+
+def d30(n):
+    if n <= 30 and n > 0:
+        return True
+    else:
+        return False
+
+def d28(n):
+    if n <= 28 and n > 0:
+        return True
+    else:
+        return False
+
+
+a, b = map(int, input().split())
+
+if a == "2":
+    if d28(b):
+        print("Yes")
+    else:
+        print("No")
+elif a == "4" or a == "6" or a == "4" or a == "9" or  a == "11":
+    if d30(b):
+        print("Yes")
+    else:
+        print("No")
+else:
+    if d31(b):
+        print("Yes")
+    else:
+        print("No")

000e4c31dd2fbb9ded3aa1870aa22e39d19da0c9
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-24 17:12:44 +0900

    Title: Next Level, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240624/Next Level/next-level.py
@@ -0,0 +1,15 @@
+s = ("codetree", 10)
+
+user, level = s
+
+print(f"user {user} lv {level}")
+
+a, b = input().split()
+
+b = int(b)
+
+s = (a, b)
+
+user, level = s
+
+print(f"user {user} lv {level}")

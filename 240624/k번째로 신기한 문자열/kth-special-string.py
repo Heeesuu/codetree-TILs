7fd08f1fab126e09bdfb666a201f96b7f16df18d
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-06-24 15:56:22 +0900

    Title: k번째로 신기한 문자열, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240624/k번째로 신기한 문자열/kth-special-string.py
+++ 240624/k번째로 신기한 문자열/kth-special-string.py
@@ -1,12 +1,18 @@
 a, b, word = map(str, input().split())
 dic = []
 
+def certi(wo, word):
+    for i in range(len(word)):
+        if wo[i] != word[i]:
+            return False
+    return True
+
 a = int(a)
 b = int(b)
 
 for i in range(a):
     wo = input()
-    if word in wo:
+    if certi(wo, word):
         dic.append(wo)
 
 dic.sort()

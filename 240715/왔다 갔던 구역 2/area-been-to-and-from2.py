7a528483a6a8a4689a9ac2389b7ced501f09b8ee
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-07-15 10:37:57 +0900

    Title: 왔다 갔던 구역 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,22 +1,22 @@
 n = int(input())
-start = 0
-arr = [0] * 200
+arr = [0] * 201  # 마이너스 영역까지 포함할 수 있도록 배열 크기 설정
+start = 100  # 시작 위치를 100으로 설정
 
 for i in range(n):
     a, b = input().split()
     a = int(a)
     if b == 'R':
-        for j in range(start, a):
+        for j in range(start, start + a):
             arr[j] += 1
         start += a
     else:
-        for k in range(start, a, -1):
+        for k in range(start - a, start):
             arr[k] += 1
         start -= a
+
 count = 0
+for x in arr:
+    if x > 1:
+        count += x - 1
 
-for j in arr:
-    if j > 1:
-        count += 1
-
-print(count//2)
+print(count-1)

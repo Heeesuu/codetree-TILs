1ce29eeca5db7571278695c8e1d76786949ac3a3
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-07-31 13:29:09 +0900

    Title: 연속되는 수 2, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = []
+
+for i in range(n):
+    a = int(input())
+    arr.append(a)
+
+le = len(arr)
+answer = []
+count = 0
+
+for i in range(le):
+    if i == 0 or arr[i] != arr[i-1]:
+        answer.append(count)
+        count = 0
+        count += 1
+    else:
+        count += 1
+
+print(max(answer))

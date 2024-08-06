3ad8aa1c5a8a8015d2a8d61ef7c2af25ccce1a4e
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-08-06 14:18:13 +0900

    Title: 벌금은 누구에게, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/벌금은 누구에게/who-will-pay.py
+++ 240806/벌금은 누구에게/who-will-pay.py
@@ -1,11 +1,16 @@
 n, m ,k = map(int, input().split())
 
 student = [0]*n
+flag = False
 
 for i in range(m):
     a = int(input())
     if student[a-1] == k-1:
         print(a)
+        flag = True
         break
     else:
         student[a-1] += 1
+
+if not flag:
+    print(-1)

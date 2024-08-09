add195a811861026c338add5fa0da1194e02ad17
Heeesuu <103115883+Heeesuu@users.noreply.github.com>
2024-08-09 09:44:13 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,24 +1,45 @@
-def simulate_infection(N, K, P, T, handshakes):
-    infected = [0] * N
-    infected[P - 1] = 1
-    handshake_count = [0] * N
-    handshakes.sort()
+# 클래스 선언
+class Shake:
+    def __init__(self, time, person1, person2):
+        self.time, self.person1, self.person2 = time, person1, person2
 
-    for t, x, y in handshakes:
-        x -= 1
-        y -= 1
+# 변수 선언 및 입력
+n, k, p, t = tuple(map(int, input().split()))	
+shakes = []
+for _ in range(t):
+    time, person1, person2 = tuple(map(int, input().split()))
+    shakes.append(Shake(time, person1, person2))
 
-        if infected[x] == 1 and handshake_count[x] < K:
-            infected[y] = 1
-            handshake_count[x] += 1
+shake_num = [0] * (n + 1)
+infected = [False] * (n + 1)
 
-        if infected[y] == 1 and handshake_count[y] < K:
-            infected[x] = 1
-            handshake_count[y] += 1
+infected[p] = True
 
-    return ''.join(map(str, infected))
+# Custom Comparator를 활용한 정렬
+shakes.sort(key = lambda x: x.time)
 
-N, K, P, T = map(int, input().split())
-handshakes = [tuple(map(int, input().split())) for _ in range(T)]
-result = simulate_infection(N, K, P, T, handshakes)
-print(result)
+# 각 악수 횟수를 세서,
+# K번 초과로 악수를 했을 시 전염시키지 않습니다.
+for shake in shakes:
+	target1 = shake.person1
+	target2 = shake.person2
+	
+	# 감염되어 있을 경우 악수 횟수를 증가시킵니다.
+	if infected[target1]:
+		shake_num[target1] += 1
+	if infected[target2]:
+		shake_num[target2] += 1
+	
+	# target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+	if shake_num[target1] <= k and infected[target1]:
+		infected[target2] = True
+	
+	# target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+	if shake_num[target2] <= k and infected[target2]:
+		infected[target1] = True
+		
+for i in range(1, n + 1):
+	if infected[i]:
+		print(1, end="")
+	else:
+		print(0, end="")
